84a8760f67c8bca5f71466bbf291cbeebd3d3353
Khubaib Ghouri <khubaibghouri@gmail.com>
2017-04-08 15:52:23 +0500

    first commit

---
+++ src/app/components/otherprofile/otherprofile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId:module.id,
+  selector: 'otheruser',
+  templateUrl: `otherprofile.component.html`,
+})
+export class OtherProfileComponent  {}
